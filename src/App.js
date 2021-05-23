5d0e017c2e6f548f5ad0ba1d7ecaef663b867a5c
darkcode01 <j0s3s3gur4@gmail.com>
2021-05-22 23:50:37 -0400

    ...

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef } from "react";
+import { Node, LinkedList } from "libs/data-structure/linked_list";
+
+const list = new LinkedList();
 
 function App() {
+  const inputRef = useRef(null);
+  const [nodes, setNodes] = useState([]);
+  const createNode = value => {
+    return new Node(value);
+  }
+
+  const addNode = () => {
+    list.append(createNode(inputRef.current.value));
+
+    clearInput();
+    updateNodes();
+  }
+
+  const updateNodes = () => setNodes(list.get_nodes());
+  const clearInput = () => inputRef.current.value = "";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <input
+          ref={inputRef}
+          type="text"
+          placeholder="valor"
+        />
+        <button onClick={addNode}>s</button>
+      </div>
+      <hr />
+      <div>
+        { nodes.map((n, index) => (
+          <li key={`${index}-node`}>{n}</li>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/libs/data-structure/linked_list.js
@@ -0,0 +1,70 @@
+// TODO: add reverse method
+
+class Node {
+  constructor(value, next=null) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null
+  }
+
+  get_last() {
+    let curr = this.head;
+
+    while (curr.next) {
+      curr = curr.next;
+    }
+
+    return curr;
+  };
+
+  append(node) {
+    if (this.head) {
+      let last = this.get_last();
+      last.next = node;
+    } else {
+      this.head = node;
+    }
+  }
+
+  prepend(node) {
+    if (this.head) {
+      let prev = this.head;
+      node.next = prev;
+      this.head = node;
+    } else {
+      this.head = node;
+    }
+  }
+
+  pop() {
+    let prev = this.head;
+    let curr = this.head.next;
+
+    while (curr.next) {
+      prev = curr;
+      curr = curr.next;
+    }
+
+    prev.next = null;
+  }
+
+  // return nodes values on array
+  get_nodes() {
+    let temp = [];
+    let curr = this.head;
+
+    while (curr) {
+      temp.push(curr.value);
+      curr = curr.next;
+    }
+
+    return temp;
+  }
+}
+
+export { LinkedList, Node };
