ac6692431ea6e22629da1fe0968dfeeade2220cb
darkcode01 <j0s3s3gur4@gmail.com>
2021-05-30 21:20:04 -0400

    feat: added simple template for design

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import Page from "@/components/Page";
+import ELContainer from "@/components/ELContainer";
+import ELStructure from "@/components/ELStructure";
+import ELEditor from "@/components/ELEditor/ELEditor";
 
 const Home = () => {
   return (
     <Page>
-      <div id="main-content">dasasadsas</div>
+      <ELContainer>
+        <ELEditor />
+        <ELStructure>
+          <ELEditor placeholder="Call Stack" className="stack" />
+          <ELEditor className="api" placeholder="Web API" />
+          <ELEditor className="queue" placeholder="Callback Queue" />
+        </ELStructure>
+      </ELContainer>
     </Page>
   );
 };
